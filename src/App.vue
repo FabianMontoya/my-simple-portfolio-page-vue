b9fd886e724000a6404207ac556472730e608c56
Fabian Dario Montoya <nachofdm@hotmail.com>
2022-08-25 23:12:22 -0500

    Create initial layout for page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import { onMounted, computed } from 'vue';
-import { useStore } from '@/store';
-
-const store = useStore();
-
-const isLoading = computed(() => store.state.photosAlbum.isLoading);
-const loadPhotosFromAlbum = () => store.dispatch('photosAlbum/loadPhotosFromAlbum', 5);
-
-onMounted(() => {
-  loadPhotosFromAlbum();
-});
+import MainLayout from '@/layout/MainLayout.vue';
+import PorfolioView from '@/views/PortFolioView.vue';
 </script>
 <template>
-  <main class="flex flex-row justify-center items-center w-full h-screen">
-    <p class="text-7xl text-red-400 icon-50px">hello world</p>
-    <p v-if="isLoading">loading...</p>
-  </main>
+  <MainLayout>
+    <PorfolioView />
+    <template v-slot:footer>
+      <p>icons</p>
+    </template>
+  </MainLayout>
 </template>
--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,6 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useMq } from 'vue3-mq';
+const mq = useMq();
+
+const isPhone = computed(() => mq.current === 'phone');
+</script>
 <template>
-  <main class="flex flex-row justify-center items-center w-full h-screen">
-    <p class="text-7xl text-red-400 icon-50px">hello world</p>
+  <main class="h-full flex flex-col justify-between">
+    <header class="h-20 flex flex-row justify-between items-center px-10">
+      <span>LOGO</span>
+      <nav class="h-11 flex items-center">
+        <ul class="nav" :class="[isPhone ? 'nav-tabs--mobile' : 'nav-tabs']">
+          <li class="nav-item active">
+            <a href="#">All</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Branding</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Web</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Photography</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">App</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Q</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <section><slot></slot></section>
+    <footer class="p-20 h-20 flex flex-col justify-between items-center">
+      <p class="text--muted text-xs">© 2016 - <span class="text--primary">Sneak</span> All Right Reserved</p>
+      <slot name="footer">Made with ❤️ by Fabian Montoya</slot>
+    </footer>
   </main>
 </template>
