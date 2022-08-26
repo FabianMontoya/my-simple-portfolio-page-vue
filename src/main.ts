c7829b36869c62f4f04748c18c627e6ae5c11ddd
Fabian Dario Montoya <nachofdm@hotmail.com>
2022-08-25 20:44:20 -0500

    add Vue3Mq package

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import { Vue3Mq } from 'vue3-mq';
+
 import App from './App.vue';
 import { store, key } from '@/store';
 import './assets/scss/main.scss';
@@ -6,6 +8,11 @@ import './assets/scss/main.scss';
 let app;
 if (!app) {
   app = createApp(App);
+
+  app.use(Vue3Mq, {
+    preset: 'devices'
+  });
   app.use(store, key);
+
   app.mount('#app');
 }
